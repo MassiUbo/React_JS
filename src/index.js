909d1c46899b88a0e8c7fd47904a7b222e368d13
Massinissa <massinissa.hjs@gmail.com>
2018-02-01 23:34:30 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
-import Request from 'react-http-request';
 
 var url = 'https://paris.demo.netdevices.fr/data'
 class App extends React.Component {
@@ -39,47 +38,38 @@ class App extends React.Component {
     };
      
     render() {
-        // affichage resultat recus
-        console.log(this.state.datanetdevice)
+        
+        if (!this.state.datanetdevice) return <p> Chargement des items... </p>
+        else {
 
-        if (!this.state.datanetdevice) return <p>Chargement des items...</p>
         if (url === "https://paris.demo.netdevices.fr/data") {
-
-            // iteration sur le tableau afin d'éviter la repetition et pouvoir 
-            // Ajout des données à l'url du site va pas affecter le code
+         
+            // iteration sur le tableau récupérer afin d'éviter la repetition et pouvoir 
+            // ajouter des données à l'url du site
             let Netdata = this.state.datanetdevice
-            let Netdatatitle = Netdata.map(item =><li> {item.title}</li>)
-            let Netdatatid= Netdata.map(item2 => item2.id)
+            let Netdatatitle = Netdata.map(item => <button onClick={e => this.test(item.id)}> {item.title}</button>)
 
             return (
                 <div>
-                    <span > Liste des élements : </span>
-              
-
-                    <button onClick={e => this.test(this.state.datanetdevice[0].id)} > {this.state.datanetdevice[0].title}  </button>
-                    <br></br>
-                    <button onClick={e => this.test(this.state.datanetdevice[1].id)}> {this.state.datanetdevice[1].title}</button>
-                    <br></br>
-                    <button onClick={e => this.test(this.state.datanetdevice[2].id)}> {this.state.datanetdevice[2].title}</button>
-                    <br></br>
-                    <button onClick={e => this.test(this.state.datanetdevice[3].id)}> {this.state.datanetdevice[1].title}</button>
-                    
-                    
-                    <ul onClick={e => this.test({Netdatatid})}> {Netdatatitle} </ul>
-                 
-                    
+                    <span > Liste des élements : </span> 
+                     {Netdatatitle}
                 </div>
             )
         }
+
+        // Détail d'un item
         else return (
+
             <div>
-                <p> Details :   !! </p>
+                <span> Détail de l'élement : </span>
                 <p>   Mon numéro d'id est :  {this.state.datanetdevice.id}       </p>
                 <p>   Ma discription est :  {this.state.datanetdevice.description}       </p>
-                <button onClick={e => this.test2()}> retour </button>
+                <button onClick={e => this.test2()}> Retour </button>
             </div>
         )
+      }
     }
+
 }
 
 
