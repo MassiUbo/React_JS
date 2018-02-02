f5886ce5c782be727cd52b1cc96a3efba2848e8e
Massinissa <massinissa.hjs@gmail.com>
2018-02-02 23:43:15 +0100

    commit 4

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         super(propos)
         this.state = {}
     }
-    // recuperation de la liste des items  
+    // recuperation de la liste des items via une url  
     componentDidMount() {
         fetch(url)
             .then(d => d.json())
@@ -26,50 +26,52 @@ class App extends React.Component {
             })
     }
 
-    // redirection vers la page detail des items
+    // récuperation de données depuis l'url detail d'un item
     test = event => {
         url = ('https://paris.demo.netdevices.fr/details?id=' + event)
         this.componentDidMount();
     };
 
+    // récuperation de données depuis l'url data
     test2 = event => {
         url = ('https://paris.demo.netdevices.fr/data')
         this.componentDidMount();
     };
-     
+
     render() {
-        
+
+        // Chargement de données
         if (!this.state.datanetdevice) return <p> Chargement des items... </p>
         else {
 
-        if (url === "https://paris.demo.netdevices.fr/data") {
-         
-            // iteration sur le tableau récupérer afin d'éviter la repetition et pouvoir 
-            // ajouter des données à l'url du site
-            let Netdata = this.state.datanetdevice
-            let Netdatatitle = Netdata.map(item => <button onClick={e => this.test(item.id)}> {item.title}</button>)
+            // si les données sont recupérés depuis l'url data    
+            if (url === "https://paris.demo.netdevices.fr/data") {
 
-            return (
+                // iteration sur le tableau récupérer afin d'éviter la repétition et pouvoir 
+                // ajouter des données à l'url du site
+                let Netdata = this.state.datanetdevice
+                let Netdatatitle = Netdata.map(item => <button onClick={e => this.test(item.id)}> {item.title}</button>)
+
+                return (
+                    <div>
+                        <span > Liste des élements : </span>
+                        {Netdatatitle}
+                    </div>
+                )
+            }
+
+            // si non les données sont récupéré depuis l'url detail
+            // retourner le détail d'un item 
+            else return (
                 <div>
-                    <span > Liste des élements : </span> 
-                     {Netdatatitle}
+                    <span> Détail de l'élement : </span>
+                    <p>   Mon numéro d'id est :  {this.state.datanetdevice.id}       </p>
+                    <p>   Ma discription est :  {this.state.datanetdevice.description}       </p>
+                    <button onClick={e => this.test2()}> Retour </button>
                 </div>
             )
         }
-
-        // Détail d'un item
-        else return (
-
-            <div>
-                <span> Détail de l'élement : </span>
-                <p>   Mon numéro d'id est :  {this.state.datanetdevice.id}       </p>
-                <p>   Ma discription est :  {this.state.datanetdevice.description}       </p>
-                <button onClick={e => this.test2()}> Retour </button>
-            </div>
-        )
-      }
     }
-
 }
 
 
